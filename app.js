03385f5ab4324f2f65bbe509acee879288ba21b4
Ayemun Hossain <37909772+AyemunHossain@users.noreply.github.com>
2023-04-25 17:28:04 +0600

    Revert "initiate"
    This reverts commit b546e96793da88d01f4cc712fd1bf1a878728293.

--- app.js
+++ app.js
@@ -1,79 +1,57 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const AWS = require('aws-sdk');
-require('aws-sdk/lib/maintenance_mode_message').suppress = true; //disable annoying worning message
-const sqsCient= require('@aws-sdk/client-sqs');
-const { Consumer } = require('sqs-consumer');
-const config = require("config");
-AWS.config.update(config);
-const sqs = new AWS.SQS({apiVersion: config.get(`apiVersion`)});
-const QueueUrl =config.get(`QueueUrl`);
+const port = 3001
 
+const firebaseConfig = {
+    "type": "service_account",
+    "project_id": "test-query-1e735",
+    "private_key_id": "----------------------",
+    "private_key": "-----BEGIN PRIVATE KEY-----\==\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-4c4pb@test-query-1e735.iam.gserviceaccount.com",
+    "client_id": "_________________",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4c4pb%40test-query-1e735.iam.gserviceaccount.com"
+  };  
 
-//Consumer section
-const consumer  = Consumer.create({
-  queueUrl: QueueUrl,
-  region: config.get(`region`),
-  handleMessageBatch: async (messages) => {
-    console.log(messages);  //It will console log the response here
-    // let body = JSON.parse(messages[0].Body);
-    // console.log({ body });
-  },
-  sqs: new sqsCient.SQSClient({
-    region: config.get(`region`),
-    credentials: {
-      accessKeyId: config.get(`accessKeyId`),
-      secretAccessKey: config.get(`secretAccessKey`),
-    }
-  }),
-});
+  const functions = require('firebase-functions');
+  // The Firebase Admin SDK to access Firestore.
+  const admin = require('firebase-admin');
+  const { getFirestore } = require("firebase-admin/firestore");
 
-app.on('error', (err) => {
-  console.error(err.message);
-});
-
-app.on('processing_error', (err) => {
-  console.error(err.message);
-});
-
-app.on('timeout_error', (err) => {
-  console.error(err.message);
-});
-
-
-app.get('/send-message', async (req, res) => {
-
-  var data = "In this object you can send the data you want";
-  sqs.sendMessage(
+  admin.initializeApp(
     {
-      MessageBody: JSON.stringify({
-        message_type: 'Test1',
-        data,
-        date: new Date().toISOString(),
-      }),
-      QueueUrl,
-    },
-    (err, data) => {
-      if (err) {
-        console.log('Error', err);
-      } else {
-        console.log("Successfully added message", data.MessageId);
-      }
-    },
-  );
+    credential:  admin.credential.cert(firebaseConfig),
+    databaseURL: "https://test-query-1e735-default-rtdb.firebaseio.com"
+    }
+    );
 
-})
+    const db = getFirestore();
 
+app.get('/', async (req, res) => {
+  
+    let ref = admin.database().ref(`/TestChatHistory/268/`).limitToLast(2);
+    ref.orderByChild("timestamp").on("value", function(data) {
+        data.forEach(function(data) {
+           console.log("The " + data.key + " rating is " + JSON.stringify(data.val()["timestamp"]));
+        })
+
+    })
 
 
+    // let ref = admin.database().ref(`/TestChatHistory/268/`).limitToFirst(2);
 
+    // ref.orderByChild("timestamp").startAfter(1680604052348).on("value", function(data) {
+    //     data.forEach(function(data) {
+    //        console.log("The " + data.key + " rating is " + JSON.stringify(new Date(data.val()["timestamp"])));
+    //     })
+    // })
 
-
+    })
 
 
 app.listen(port, () => {
-  consumer.start();
   console.log(`Example app listening on port ${port}`)
 })
 
